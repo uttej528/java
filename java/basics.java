393ac59322c104be4a8d563ee09ddcd9037daf75
uttej528 <96988310+uttej528@users.noreply.github.com>
2023-01-31 11:35:47 +0530

    Add files via upload

---
+++ java/basics.java
@@ -0,0 +1,13 @@
+
+public class basics{
+
+
+  public static void main(String[] args )
+  {
+
+int num1=100; int num2=200; int result;
+result=num1*num2;
+System.out.println("Result is :" + result);
+
+  }
+}
